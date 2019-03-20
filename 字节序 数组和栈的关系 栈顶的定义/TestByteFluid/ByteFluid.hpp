2e9a48e788b358d9c345898638635680867f2c6e
15110671453 <15110671453@163.com>
2019-03-20 10:17:11 +0800

    字节序 记录
    字节序 记录

---
+++ 字节序 数组和栈的关系 栈顶的定义/TestByteFluid/ByteFluid.hpp
@@ -0,0 +1,26 @@
+//
+//  ByteFluid.hpp
+//  TestByteFluid
+//
+//  Created by admindyn on 2017/12/23.
+//  Copyright © 2017年 admindyn. All rights reserved.
+//
+
+#ifndef ByteFluid_hpp
+#define ByteFluid_hpp
+
+#include <stdio.h>
+
+class ByteFluid
+
+{
+public:
+    ByteFluid();
+    
+    ~ByteFluid();
+
+};
+
+
+
+#endif /* ByteFluid_hpp */
---
+++ 字节序 数组和栈的关系 栈顶的定义/TestByteFluid/TestByteFluid/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  TestByteFluid
+//
+//  Created by admindyn on 2017/12/23.
+//  Copyright © 2017年 admindyn. All rights reserved.
+//
+
+#include "ByteFluid.hpp"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    ByteFluid* fByte = new ByteFluid();
+    ByteFluid fByte2;
+    return 0;
+}
